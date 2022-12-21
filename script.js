7f395c79ad8ef348f2cd32274dbddfdf610466a9
DAMIAN RIVERA <>
2022-12-21 19:14:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let title = document.querySelector('.title')
+let storyOpening = document.querySelector('.story-opening');
+let optionZeroButton = document.querySelector('.option-zero');
+let optionOneScreen = document.querySelector('.option-one-screen');
+let optionTwoScreen = document.querySelector('.option-two-screen');
+let optionOneButton = document.querySelector('.option-one-button');
+optionZeroButton.onclick = function(){
+  storyOpening.style.display = 'block';
+  title.style.display = 'none';
+
+}
+
+
+
+
+storyOpening.onclick=function(){
+storyOpening.style.display = 'none'
+optionOneScreen.style.display = 'block';
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
